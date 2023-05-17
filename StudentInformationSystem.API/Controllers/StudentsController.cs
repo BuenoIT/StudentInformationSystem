da4562526e59feefe4c7bae96acc4b5c9a629bbc
BuenoIT <bueno.itdev@gmail.com>
2023-05-17 13:48:58 -0700

    Display Single Student Data Implementation

--- StudentInformationSystem.API/Controllers/StudentsController.cs
+++ StudentInformationSystem.API/Controllers/StudentsController.cs
@@ -25,5 +25,21 @@ namespace StudentInformationSystem.API.Controllers
 
             return Ok(mapper.Map<List<Student>>(students));
         }
+
+        [HttpGet]
+        [Route("[controller]/{studentId:guid}")]
+        public async Task<IActionResult> GetStudentAsync([FromRoute] Guid studentId)
+        {
+            //Fetch single student detail
+            var student = await studentRepository.GetStudentAsync(studentId);
+
+            //Return student
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(mapper.Map<Student>(student));
+        }
     }
 }
